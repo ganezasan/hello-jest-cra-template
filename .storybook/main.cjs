4cef9220dda0f349d68658200824f35a1e80b08e
ganezasan <istaka12@gmail.com>
2023-02-15 08:46:59 +0900

    use webpack5 and type: module

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,12 +1,33 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    '@storybook/preset-create-react-app',
   ],
-  "framework": "@storybook/react",
+  framework: "@storybook/react",
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  features: {
+    interactionsDebugger: true
+  },
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ]
+    config.module.rules.push({
+      test: /.storybook\/preview.ts/,
+      resolve: { fullySpecified: false },
+    })
+    return config
+  },
 }
